5c02333267ae6aa578e921151e648091c73d8537
Dawid Małyska <dawid.malyska@interia.pl>
2026-04-26 10:03:27 +0200

    :wq

--- analyze_heart_data.py
+++ analyze_heart_data.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
-
+ 
+ 
 def load_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
-
-
+ 
+ 
 def basic_report(df: pd.DataFrame) -> dict:
     return {
         'rows': len(df),
@@ -12,9 +12,9 @@ def basic_report(df: pd.DataFrame) -> dict:
         'missing_values': int(df.isna().sum().sum()),
         'target_distribution': df['target'].value_counts().to_dict() if 'target' in df.columns else {}
     }
-
-
+ 
+ 
 if __name__ == '__main__':
-    url = 'https://raw.githubusercontent.com/plotly/datasets/master/heart.csv'
-    data = load_data(url)
+    path = 'data\\Data.csv'
+    data = load_data(path)
     print(basic_report(data))
